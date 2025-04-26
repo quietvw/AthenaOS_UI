4db8a7c133ee9998f918837519cd4d93962aeb11
Thomas Wilbur <twilbur@adaclare.com>
2025-04-26 02:07:46 -0500

    Time/Weather

--- main.py
+++ main.py
@@ -124,4 +124,4 @@ def public_files(filename):
 # Start the server
 # -------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=15500, debug=True)
+    app.run(host="127.0.0.1", port=15500, debug=True)
